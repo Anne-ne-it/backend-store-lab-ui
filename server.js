a834c66c3fe7e1df5350aa7929c74c7f29d5a515
Ane Nemo <anne.aikau@gmail.com>
2026-09-01 17:08:18 +0200

    backend

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-import express from "express"
-import cors from "cors"
+import "dotenv/config";
 
-const app = express()
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(
-  cors({
-    origin: "http://localhost:5174",
-    credentials: true,
-  })
-)
+cors({
+origin: "http://localhost:5174",
+credentials: true,
+})
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+res.json({
+message: "StoreLab API funcionando",
+});
+});
+
+app.listen(PORT, () => {
+console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
