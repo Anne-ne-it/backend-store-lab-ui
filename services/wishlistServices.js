c88da171ff6e386d2644aa2d2f7772d43f7bf273
Ane Nemo <anne.aikau@gmail.com>
2026-09-18 16:01:10 +0200

    wishlist

--- services/wishlistServices.js
+++ services/wishlistServices.js
@@ -1,4 +1,4 @@
-import { Wishlist } from "../models/Wishlist.js"
+import { Wishlist } from "../models/wishlist.js"
 
 export const addToWishlist = async (userId, productId) => {
   const wishlist = new Wishlist({ userId, productId })
